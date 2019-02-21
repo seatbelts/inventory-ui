3db54b7ad7f8c4c4fce6b9792b4212fdf871b2fc
Ismael Valdez <seatbelts@Ismaels-MacBook-Pro.local>
2019-02-20 22:29:29 -0600

    added routing and lazy loading to application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'items',
+    loadChildren: './items/items.module#ItemsModule'
+  },
+  {
+    path: '',
+    redirectTo: '/items',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppConfigService } from './shared/app-config.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+const appInitializer = (appConfig: AppConfigService) => {
+  return () => {
+      return appConfig.load();
+  };
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +18,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    AppConfigService,
+    {
+        provide: APP_INITIALIZER,
+        useFactory: appInitializer,
+        multi: true,
+        deps: [AppConfigService]
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
