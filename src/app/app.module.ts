d328246db77f32a71baa0d325c7a758092bf20cb
Valdez Gomez <ismael.valdez.gomez@accenture.com>
2019-02-21 08:55:31 -0600

    added custom http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { CustomHttpService } from 'src/app/core/custom-http.service';
 import { AppConfigService } from './shared/app-config.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 const appInitializer = (appConfig: AppConfigService) => {
   return () => {
       return appConfig.load();
   };
 };
+
+export function customHttpFactory(http: HttpClient) {
+  return new CustomHttpService(http);
+}
 
 @NgModule({
   declarations: [
@@ -29,7 +35,13 @@ const appInitializer = (appConfig: AppConfigService) => {
         useFactory: appInitializer,
         multi: true,
         deps: [AppConfigService]
-    }],
+    },
+    {
+      provide: CustomHttpService,
+      useFactory: customHttpFactory,
+      deps: [HttpClient]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
